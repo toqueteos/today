61f4f3fdcb065a93439ed305a2deb27d5dee1bb7
Carlos Cobo <toqueteos@gmail.com>
2016-06-03 11:17:25 +0200

    Add timezone-awareness
    - Add: new `-location` flag, which defaults to 'Europe/Madrid'
    - Fix: time.Sub now gets the proper number of seconds

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 	"time"
 )
 
@@ -10,18 +11,26 @@ var (
 	flagMoney       float64
 	flagStartHour   int
 	flagJobDuration time.Duration
+	flagLocation    string
 )
 
 func main() {
 	flag.Float64Var(&flagMoney, "money", 130.00, "your earnings per day")
 	flag.IntVar(&flagStartHour, "start-hour", 8, "hour of the day you start working, this will change in the future")
 	flag.DurationVar(&flagJobDuration, "duration", time.Hour*8, "hours per day you work")
+	flag.StringVar(&flagLocation, "location", "Europe/Madrid", "location name as in IANA Time Zone database")
 	flag.Parse()
+
+	loc, err := time.LoadLocation(flagLocation)
+	if err != nil {
+		fmt.Printf("Invalid location %q\n", flagLocation)
+		os.Exit(1)
+	}
 
 	now := time.Now()
 
 	// TODO: Customize starting hour. Example: -start 8:30
-	start := time.Date(now.Year(), now.Month(), now.Day(), flagStartHour, 0, 0, 0, time.UTC)
+	start := time.Date(now.Year(), now.Month(), now.Day(), flagStartHour, 0, 0, 0, loc)
 
 	// TODO: Use math/big instead of float64s
 	totalSeconds := flagJobDuration.Seconds()
